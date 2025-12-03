67aa21836a1dd6f7ab0e02ef09df7ec2399005d5
Italo Mendoza <105523522+syderkkk@users.noreply.github.com>
2025-12-03 14:06:56 -0500

    Enrutamiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RootLayout from './layouts/RootLayout';
+import Home from './pages/Home';
+import Entities from './pages/Entities';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element> 
-          <Route>
-
-          </Route>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="entities" element={<Entities />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
