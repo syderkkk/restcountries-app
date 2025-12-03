d2668334c79d956194b193c2a3e0d51639ed8869
Italo Mendoza <105523522+syderkkk@users.noreply.github.com>
2025-12-03 14:15:55 -0500

    configuracion de RootLayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const RootLayout = () => {
     return (
         <div>
+            <Header />
             <main>
                 <Outlet />
             </main>
+            <Footer />
         </div>
     );
 }
- 
+
 export default RootLayout;
