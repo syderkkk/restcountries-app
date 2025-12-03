c5074b515e7c7fcb7661277c33ff21769a6da051
Italo Mendoza <105523522+syderkkk@users.noreply.github.com>
2025-12-03 13:55:22 -0500

    proyecto inicial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element> 
+          <Route>
+
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
