42ee2e3d876db7c5bfd19f31ab600203204eea75
Italo Mendoza <105523522+syderkkk@users.noreply.github.com>
2025-12-03 14:07:43 -0500

    add RootLayout

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const RootLayout = () => {
+    return (
+        <div>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+}
+ 
+export default RootLayout;
